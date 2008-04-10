c593f97bbd05e1f9fdba543e1196c81fcd1d2f1d
phillemann <pmidden@gmx.net>
2008-04-10 21:38:29 +0200

    added resolution file

---
+++ resolution.cpp
@@ -0,0 +1,6 @@
+#include "resolution.hpp"
+
+sge::screen_size_t sanguis::resolution()
+{
+	return sge::screen_size_t(640,480);
+}
---
+++ resolution.hpp
@@ -0,0 +1,11 @@
+#ifndef SANGUIS_RESOLUTION_HPP_INCLUDED
+#define SANGUIS_RESOLUTION_HPP_INCLUDED
+
+#include <sge/renderer/types.hpp>
+
+namespace sanguis
+{
+sge::screen_size_t resolution();
+}
+
+#endif
