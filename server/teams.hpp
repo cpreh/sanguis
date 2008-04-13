e416edc486cdf936fc9c6288cfb6feaee1e2bac3
phillemann <pmidden@gmx.net>
2008-04-13 20:39:42 +0200

    added missing file

---
+++ server/teams.hpp
@@ -0,0 +1,15 @@
+#ifndef SANGUIS_SERVER_TEAMS_HPP_INCLUDED
+#define SANGUIS_SERVER_TEAMS_HPP_INCLUDED
+
+namespace sanguis
+{
+namespace server
+{
+namespace team
+{
+	enum type { players,monsters };
+}
+}
+}
+
+#endif
