e7c87ac7303085e4011e554fe642309c65a7d61c
Philipp Reh <sefi@s-e-f-i.de>
2008-05-09 21:16:45 +0200

    Added pause messages.

---
+++ messages/unpause.hpp
@@ -0,0 +1,21 @@
+#ifndef SANGUIS_MESSAGES_UNPAUSE_HPP_INCLUDED
+#define SANGUIS_MESSAGES_UNPAUSE_HPP_INCLUDED
+
+#include "base.hpp"
+#include <boost/serialization/access.hpp>
+
+namespace sanguis
+{
+namespace messages
+{
+
+class unpause : public base {
+	friend class boost::serialization::access;
+	template<typename Archive>
+	void serialize(Archive &ar, unsigned);
+};
+
+}
+}
+
+#endif
