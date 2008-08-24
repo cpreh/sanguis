69bedd0c07ede14dab362202f5e274d4310eca4e
Philipp Reh <sefi@s-e-f-i.de>
2008-08-24 21:41:24 +0200

    Added more waves for fun.

--- server/waves/generator.cpp
+++ server/waves/generator.cpp
@@ -1,27 +1,63 @@
 #include "generator.hpp"
 #include "simple.hpp"
+#include <sge/time/second.hpp>
 
 sanguis::server::waves::generator::generator()
+: diff_clock_(),
+  wave_timer(
+  	sge::time::second(
+		static_cast<sge::time::unit>(20)),
+	true,
+	diff_clock_.callback()),
+  wave_counter(0)
 {
 	// TODO: make this more flexible!
 	waves.push_back(
 		new waves::simple(
 			static_cast<time_type>(0.5),
-			100,
+			200,
 			enemy_type::zombie00));
-	
-	waves.push_back(
-		new waves::simple(
-			static_cast<time_type>(1),
-			50,
-			enemy_type::zombie01));
-
 }
 
 void sanguis::server::waves::generator::process(
 	time_type const diff,
 	environment const &env)
 {
+	diff_clock_.update(diff);
+
+	if(wave_timer.update_b())
+	{
+		switch(wave_counter) {
+		case 0:
+			waves.push_back(
+				new waves::simple(
+					static_cast<time_type>(1),
+					50,
+					enemy_type::zombie01));
+			break;
+		case 1:
+			waves.push_back(
+				new waves::simple(
+					static_cast<time_type>(0.8),
+					30,
+					enemy_type::wolf_black));
+		case 2:
+			waves.push_back(
+				new waves::simple(
+					static_cast<time_type>(2),
+					10,
+					enemy_type::wolf_white));
+		case 3:
+			waves.push_back(
+				new waves::simple(
+					static_cast<time_type>(1),
+					20,
+					enemy_type::wolf_brown));
+		}
+		if(wave_counter < 3)
+			++wave_counter;
+	}
+
 	for(wave_list::iterator it = waves.begin(); it != waves.end(); )
 	{
 		it->process(
--- server/waves/generator.hpp
+++ server/waves/generator.hpp
@@ -3,6 +3,8 @@
 
 #include "wave.hpp"
 #include "../../time_type.hpp"
+#include "../../diff_clock.hpp"
+#include <sge/time/timer.hpp>
 #include <boost/noncopyable.hpp>
 #include <boost/ptr_container/ptr_list.hpp>
 
@@ -23,6 +25,9 @@ public:
 		time_type,
 		environment const &);
 private:
+	diff_clock         diff_clock_;
+	sge::time::timer   wave_timer;
+	unsigned           wave_counter;
 	typedef boost::ptr_list<wave> wave_list;
 	wave_list waves;
 };
