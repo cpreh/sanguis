6b4dc561a2d1caa1de1ded41a92dbc6244bc2424
phillemann <pmidden@gmx.net>
2008-09-11 17:15:37 +0200

    shader effect now working

--- fragment.glsl
+++ fragment.glsl
@@ -7,12 +7,14 @@ void main()
 		expltex,
 		gl_TexCoord[0].st);
 
-	vec2 add = normalize(gl_TexCoord[1].st - expl_center) * expl.x;
+	vec2 add = normalize(gl_TexCoord[1].st - expl_center) * expl.x * 100;
 	vec2 final_pos = vec2(gl_TexCoord[1]) + add;
+
+	vec2 transformed = final_pos/1024.0;
 
 	vec4 final = texture2D(
 		grasstex,
-		final_pos);
+		transformed);
 
 	gl_FragColor = final;
 }
