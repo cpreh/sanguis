6d677282844b00893e67c96df6e8ec530ae8afb3
phillemann <pmidden@gmx.net>
2008-10-07 21:51:37 +0200

    hacked rng

--- load/resource/sound.cpp
+++ load/resource/sound.cpp
@@ -37,7 +37,7 @@ sge::audio::sound_ptr const sanguis::load::resource::environment::load_sound(
 	static sge::random::uniform<sound_container::size_type> 
 		rng(
 			static_cast<sound_container::size_type>(0),
-			s.size());
+			s.size()+1);
 
 	if (s.size())
 	{
