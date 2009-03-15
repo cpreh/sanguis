676be1a6c81cbe26e824818dae9d9d8cd48b59c5
Philipp Reh <sefi@s-e-f-i.de>
2009-03-15 17:59:30 +0100

    Get cell_size from a global ini.

--- load/model/model.cpp
+++ load/model/model.cpp
@@ -35,12 +35,102 @@ bool header_equal(
 	return seq.header == header_name;
 }
 
+// FIXME: make this work with wide strings, too!
+typedef sge::ifstream ifstream;
+
+typedef std::istream_iterator<
+	ifstream::char_type
+> iterator;
+
+sge::parse::ini::grammar<
+	iterator
+> parser;
+
+sge::renderer::dim_type const
+load_dim(
+	sge::filesystem::path const &path)
+{
+	sge::filesystem::path const ini_file(
+		path / SGE_TEXT("global.ini")
+	);
+
+	ifstream ifs(
+		ini_file
+	);
+
+	if(!ifs.is_open())
+		throw sanguis::exception(
+			SGE_TEXT("Failed to open ini file: ")
+			+ ini_file.string()
+		);
+
+	iterator beg(
+		ifs
+	);
+
+	sge::parse::ini::section_vector sections;
+
+	if(
+		!sge::parse::ini::parse(
+			beg,
+			iterator(),
+			sections
+		)
+	)
+	{
+		SGE_LOG_WARNING(
+			sanguis::load::log(),
+			sge::log::_1
+				<< ini_file.string()
+				<< SGE_TEXT(" contains errors!")
+			);
+	}
+	
+	sge::parse::ini::section_vector::const_iterator const header_it(
+		std::find_if(
+			sections.begin(),
+			sections.end(),
+			std::ptr_fun(
+				header_equal
+			)
+		)
+	);
+
+	if(header_it == sections.end())
+		throw sanguis::exception(
+			SGE_TEXT("header subsection not found in ")
+			+ ini_file.string()
+		);
+
+	sge::parse::ini::section const header(
+		*header_it
+	);
+
+
+	return sge::renderer::dim_type(
+		sanguis::load::model::get_entry<int>(
+			header.entries,
+			SGE_TEXT("cell_width")
+		),
+		sanguis::load::model::get_entry<int>(
+			header.entries,
+			SGE_TEXT("cell_height")
+		)
+	);
+}
+
 }
 
 sanguis::load::model::model::model(
 	sge::filesystem::path const &path,
 	resource::context const &ctx)
 {
+	sge::renderer::dim_type const cell_size(
+		load_dim(
+			path
+		)
+	);
+
 	for(sge::filesystem::directory_iterator beg(path), end; beg != end; ++beg)
 	{
 		sge::filesystem::path const &file(
@@ -66,8 +156,6 @@ sanguis::load::model::model::model(
 			)
 		);
 
-		// FIXME: make this work with wide strings, too!
-		typedef sge::ifstream ifstream;
 
 		sge::filesystem::path const ini_file(
 			path / sge::filesystem::stem(file) / SGE_TEXT(".ini")
@@ -89,15 +177,6 @@ sanguis::load::model::model::model(
 			continue;
 		}
 
-		typedef std::istream_iterator<
-			ifstream::char_type
-		> iterator;
-
-		sge::parse::ini::grammar<
-			iterator
-		> parser;
-
-			
 		iterator beg(
 			ifs
 		);
@@ -120,52 +199,11 @@ sanguis::load::model::model::model(
 				);
 		}
 
-		sge::parse::ini::section_vector::const_iterator const header_it(
-			std::find_if(
-				sections.begin(),
-				sections.end(),
-				std::ptr_fun(
-					header_equal
-				)
-			)
-		);
-
-		if(header_it == sections.end())
-		{
-			SGE_LOG_WARNING(
-				log(),
-				sge::log::_1
-					<< SGE_TEXT("header subsection not found in ")
-					<< ini_file
-			);
-			
-			continue;
-		}
-
-		sge::parse::ini::section const header(
-			*header_it
-		);
-
-		sge::renderer::dim_type const cell_size(
-			get_entry<int>(
-				header.entries,
-				SGE_TEXT("cell_width")
-			),
-			get_entry<int>(
-				header.entries,
-				SGE_TEXT("cell_height")
-			)
-		);
-		
-
 		BOOST_FOREACH(
 			sge::parse::ini::section_vector::const_reference section,
 			sections
 		)
 		{	
-			if(section.header == header_name)
-				continue;
-
 			split_pair const names(
 				split_first_slash(
 					section.header
