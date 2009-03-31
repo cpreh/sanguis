fac11c40cb794a62371485dee55ad2f26659d6a7
Philipp Reh <sefi@s-e-f-i.de>
2009-03-31 23:01:24 +0200

    Added more message types.

--- messages/types/message.hpp
+++ messages/types/message.hpp
@@ -12,9 +12,38 @@ namespace message
 {
 enum type {
 	add,
+	add_decoration,
+	add_enemy,
+	add_pickup,
+	add_projectile,
+	add_weapon_pickup,
+	assign_id,
+	available_perks,
+	change_weapon,
+	client_info,
 	connect,
 	disconnect,
+	experience,
+	give_weapon,
+	health,
+	level_up,
+	max_health,
+	move,
 	pause,
+	player_change_weapon.
+	player_choose_perk,
+	player_direction,
+	player_pause,
+	player_rotation,
+	player_start_attacking,
+	player_stop_attacking,
+	player_unpause,
+	remove,
+	resize,
+	rotate,
+	speed,
+	start_attacking,
+	stop_attacking,
 	unpause
 };
 }
