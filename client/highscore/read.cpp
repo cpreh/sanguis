a3644f886eb8db3a980778d7df0b00fdcd03d4f4
Philipp Reh <sefi@s-e-f-i.de>
2009-06-07 16:31:05 +0200

    Added highscore/read/write.

--- client/highscore/read.cpp
+++ client/highscore/read.cpp
@@ -1,18 +1,110 @@
 #include "read.hpp"
+#include "../log.hpp"
+#include <sge/parse/json/parse_range.hpp>
+#include <sge/parse/json/object.hpp>
+#include <sge/parse/json/array.hpp>
+#include <sge/parse/json/get.hpp>
+#include <sge/parse/json/find_member.hpp>
 #include <sge/assign/make_container.hpp>
+#include <sge/log/headers.hpp>
+#include <sge/string.hpp>
+#include <sge/char_type.hpp>
 #include <sge/text.hpp>
+#include <boost/foreach.hpp>
+#include <istream>
 
-sanguis::client::highscore::table const sanguis::client::highscore::read(
-	sge::istream &)
+sanguis::client::highscore::table const
+sanguis::client::highscore::read(
+	sge::istream &stream)
 {
+	sge::parse::json::object result;
+
+	{
+		// TODO: can't we use the istream iterators directly?
+		sge::string str;
+
+		sge::char_type ch;
+
+		while(stream.get(ch))
+			str.push_back(ch);
+
+		sge::string::const_iterator beg(
+			str.begin()
+		);
+
+		if(
+			!sge::parse::json::parse_range(
+				beg,
+				static_cast<
+					sge::string::const_iterator
+				>(
+					str.end()
+				),
+				result
+			)
+		)
+		{
+			SGE_LOG_ERROR(
+				log(),
+				sge::log::_1
+					<< SGE_TEXT("Errors in the highscore file detected!")
+			);
+
+			return table();
+		}
+	}
+
 	table t;
-	t.push_back(
-		entry(
-			sge::assign::make_container<name_container>(SGE_TEXT("Phillemann"))(SGE_TEXT("Freundlich")),
-			31337));
-	t.push_back(
-		entry(
-			sge::assign::make_container<name_container>(SGE_TEXT("nille")),
-			1337));
+
+	BOOST_FOREACH(
+		sge::parse::json::element_vector::const_reference elem,
+		sge::parse::json::find_member<
+			sge::parse::json::array
+		>(
+			result.members,
+			SGE_TEXT("entries")
+		).elements
+	)
+	{
+		sge::parse::json::member_vector const &members(
+			sge::parse::json::get<
+				sge::parse::json::object
+			>(
+				elem
+			).members
+		);
+
+		name_container names_result;
+
+		BOOST_FOREACH(
+			sge::parse::json::element_vector::const_reference name,
+			sge::parse::json::find_member<
+				sge::parse::json::array
+			>(
+				members,
+				SGE_TEXT("names")
+			).elements
+		)
+			names_result.push_back(
+				sge::parse::json::get<
+					sge::parse::json::string
+				>(
+					name
+				)
+			);
+		
+		t.push_back(
+			entry(
+				names_result,
+				sge::parse::json::find_member<
+					int
+				>(
+					members,
+					SGE_TEXT("score")
+				)
+			)
+		);
+	}
+
 	return t;
 }
--- client/highscore/write.cpp
+++ client/highscore/write.cpp
@@ -1,2 +1,56 @@
 #include "write.hpp"
+#include <sge/text.hpp>
+#include <boost/next_prior.hpp>
+#include <ostream>
 
+void
+sanguis::client::highscore::write(
+	sge::ostream &s,
+	table const &t)
+{
+	s << SGE_TEXT("{ \"entries\": [");
+
+	for(
+		table::const_iterator it(
+			t.begin()
+		);
+		it != t.end();
+		++it
+	)
+	{
+		s << SGE_TEXT("{ \"names\": [");
+
+		name_container const &names(
+			it->names()
+		);
+
+		for(
+			name_container::const_iterator nit(
+				names.begin()
+			);
+			nit != names.end();
+			++nit
+		)
+		{
+			s
+				<< SGE_TEXT('"')
+				<< *nit
+				<< SGE_TEXT('"');
+
+			if(boost::next(nit) != names.end())
+				s << SGE_TEXT(',');
+		}
+
+		s
+			<< SGE_TEXT("], score : ")
+			<< it->score()
+			<< SGE_TEXT('}');
+
+		s << SGE_TEXT('}');
+
+		if(boost::next(it) != t.end())
+			s << SGE_TEXT(',');
+	}
+
+	s << SGE_TEXT("]}");
+}
