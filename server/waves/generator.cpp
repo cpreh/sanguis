c70a838a293aa5596842a36226038d0b9119b138
Philipp Reh <sefi@s-e-f-i.de>
2010-02-14 23:27:15 +0100

    Added spawn_all command.

--- server/waves/generator.cpp
+++ server/waves/generator.cpp
@@ -4,7 +4,6 @@
 #include "make.hpp"
 #include "convert_enemy_name.hpp"
 #include "../../exception.hpp"
-//#include "debug.hpp"
 #include <sge/console/object.hpp>
 #include <fcppt/tr1/functional.hpp>
 #include <fcppt/text.hpp>
@@ -26,11 +25,124 @@ sanguis::server::waves::generator::generator(
 				this,
 				std::tr1::placeholders::_1
 			),
-			FCPPT_TEXT("spawn wave [wavename] or spawn enemy [enemyname]")
+			FCPPT_TEXT("spawn wave [wavename], spawn enemy [enemyname] or spawn all")
 		)
 	)
+{}
+
+sanguis::server::waves::generator::~generator()
+{}
+
+void
+sanguis::server::waves::generator::process(
+	time_type const diff,
+	environment::object_ptr const env,
+	environment::load_context_ptr const load_context
+)
 {
-#if 0
+	for(
+		wave_list::iterator it = waves.begin();
+		it != waves.end();
+	)
+	{
+		it->process(
+			diff,
+			env,
+			load_context
+		);
+
+		if(it->ended())
+			it = waves.erase(it);
+		else
+			++it;
+	}
+}
+
+void
+sanguis::server::waves::generator::spawn(
+	sge::console::arg_list const &args_
+)
+try
+{
+	if(
+		args_.size() == 2u
+		&& args_[1] == FCPPT_TEXT("all")
+	)
+	{
+		spawn_all();
+
+		return;
+	}
+
+	if(
+		args_.size() != 3u
+	)
+	{
+		FCPPT_LOG_ERROR(
+			log(),
+			fcppt::log::_
+				<< FCPPT_TEXT("Invalid call to spawn!")
+		);
+
+		// TODO: error!
+		return;
+	}
+
+	fcppt::string const action(
+		args_[1]
+	);
+
+	if(
+		action == FCPPT_TEXT("wave")
+	)
+		waves.push_back(
+			make(
+				args_[2]
+			)
+		);
+	else if(
+		action == FCPPT_TEXT("enemy")
+	)
+	{
+		wave_auto_ptr ptr(
+			fcppt::make_auto_ptr<
+				single
+			>(
+				convert_enemy_name(
+					args_[2]
+				)
+			)
+		);
+
+		waves.push_back(
+			ptr
+		);
+	}
+	else
+	{
+		FCPPT_LOG_ERROR(
+			log(),
+			fcppt::log::_
+				<< FCPPT_TEXT("Invalid argument")
+		);
+		// TODO: error!
+	}
+}
+catch(
+	exception const &e
+)
+{
+	FCPPT_LOG_ERROR(
+		log(),
+		fcppt::log::_
+			<< e.string()
+	);
+	// TODO: error!
+}
+
+void
+sanguis::server::waves::generator::spawn_all()
+{
 	// TODO: somehow put this in a configuration file!
 	boost::assign::ptr_push_back<waves::infinite>(waves)
 	(
@@ -94,109 +206,4 @@ sanguis::server::waves::generator::generator(
 		spawns_per_wave(1),
 		enemy_type::skeleton
 	);
-
-	//boost::assign::ptr_push_back<waves::debug>(waves)
-	//();
-#endif
-}
-
-sanguis::server::waves::generator::~generator()
-{}
-
-void
-sanguis::server::waves::generator::process(
-	time_type const diff,
-	environment::object_ptr const env,
-	environment::load_context_ptr const load_context
-)
-{
-	for(
-		wave_list::iterator it = waves.begin();
-		it != waves.end();
-	)
-	{
-		it->process(
-			diff,
-			env,
-			load_context
-		);
-
-		if(it->ended())
-			it = waves.erase(it);
-		else
-			++it;
-	}
-}
-
-void
-sanguis::server::waves::generator::spawn(
-	sge::console::arg_list const &args_
-)
-try
-{
-	if(
-		args_.size() != 3u
-	)
-	{
-		FCPPT_LOG_ERROR(
-			log(),
-			fcppt::log::_
-				<< FCPPT_TEXT("Invalid argument count ")
-				<< args_.size()
-		);
-
-		// TODO: error!
-		return;
-	}
-
-	fcppt::string const action(
-		args_[1]
-	);
-
-	if(
-		action == FCPPT_TEXT("wave")
-	)
-		waves.push_back(
-			make(
-				args_[2]
-			)
-		);
-	else if(
-		action == FCPPT_TEXT("enemy")
-	)
-	{
-		wave_auto_ptr ptr(
-			fcppt::make_auto_ptr<
-				single
-			>(
-				convert_enemy_name(
-					args_[2]
-				)
-			)
-		);
-
-		waves.push_back(
-			ptr
-		);
-	}
-	else
-	{
-		FCPPT_LOG_ERROR(
-			log(),
-			fcppt::log::_
-				<< FCPPT_TEXT("Invalid argument")
-		);
-		// TODO: error!
-	}
-}
-catch(
-	exception const &e
-)
-{
-	FCPPT_LOG_ERROR(
-		log(),
-		fcppt::log::_
-			<< e.string()
-	);
-	// TODO: error!
 }
