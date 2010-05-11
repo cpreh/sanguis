0b297eb33a3cf6c183de18c20f24846465ff2b50
Philipp Reh <sefi@s-e-f-i.de>
2010-05-11 20:30:08 +0200

    Handle changes in sge::config::find_path.

--- media_path.cpp
+++ media_path.cpp
@@ -1,14 +1,19 @@
 #include "media_path.hpp"
+#include "exception.hpp"
 #include <fcppt/filesystem/path.hpp>
 #include <sge/config/find_path.hpp>
 #include <sge/config/make_files.hpp>
+#include <fcppt/optional_impl.hpp>
 #include <fcppt/text.hpp>
 #include <boost/assign/list_of.hpp>
 
-fcppt::filesystem::path const
-sanguis::media_path()
+namespace
 {
-	static fcppt::filesystem::path const ret(
+
+fcppt::filesystem::path const
+find_path()
+{
+	sge::config::optional_path const path_(
 		sge::config::find_path(
 			sge::config::make_files(
 				FCPPT_TEXT("sanguis")
@@ -21,6 +26,25 @@ sanguis::media_path()
 				MEDIA_PATH
 			)
 		)
+	);
+
+	if(
+		!path_
+	)
+		throw sanguis::exception(
+			FCPPT_TEXT("media path not found!")
+		);
+	
+	return *path_;
+}
+
+}
+
+fcppt::filesystem::path const
+sanguis::media_path()
+{
+	static fcppt::filesystem::path const ret(
+		find_path()
 	);
 
 	return ret; 
