fae52e430418d187c1c790baffaff49f584b044d
Philipp Reh <sefi@s-e-f-i.de>
2010-05-20 22:17:48 +0200

    Use find_path_exn.

--- media_path.cpp
+++ media_path.cpp
@@ -1,20 +1,15 @@
 #include "media_path.hpp"
-#include "exception.hpp"
 #include <fcppt/filesystem/path.hpp>
-#include <sge/config/find_path.hpp>
+#include <sge/config/find_path_exn.hpp>
 #include <sge/config/make_files.hpp>
-#include <fcppt/optional_impl.hpp>
 #include <fcppt/text.hpp>
 #include <boost/assign/list_of.hpp>
 
-namespace
-{
-
 fcppt::filesystem::path const
-find_path()
+sanguis::media_path()
 {
-	sge::config::optional_path const path_(
-		sge::config::find_path(
+	static fcppt::filesystem::path const ret(
+		sge::config::find_path_exn(
 			sge::config::make_files(
 				FCPPT_TEXT("sanguis")
 			),
@@ -26,25 +21,6 @@ find_path()
 				MEDIA_PATH
 			)
 		)
-	);
-
-	if(
-		!path_
-	)
-		throw sanguis::exception(
-			FCPPT_TEXT("media path not found!")
-		);
-	
-	return *path_;
-}
-
-}
-
-fcppt::filesystem::path const
-sanguis::media_path()
-{
-	static fcppt::filesystem::path const ret(
-		find_path()
 	);
 
 	return ret; 
