235295e3f78e2dd51a67635c17f51351fa6ad29c
Philipp Reh <sefi@s-e-f-i.de>
2011-06-24 20:59:28 +0200

    Fix for mizuiro.

--- src/client/draw2d/sunlight/rgb32f_format.hpp
+++ src/client/draw2d/sunlight/rgb32f_format.hpp
@@ -2,7 +2,7 @@
 #define SANGUIS_CLIENT_DRAW2D_SUNLIGHT_RGB32F_FORMAT_HPP_INCLUDED
 
 #include "real.hpp"
-#include <mizuiro/color/homogenous.hpp>
+#include <mizuiro/color/homogenous_static.hpp>
 #include <mizuiro/color/layout/rgb.hpp>
 
 namespace sanguis
@@ -14,7 +14,7 @@ namespace draw2d
 namespace sunlight
 {
 
-typedef mizuiro::color::homogenous<
+typedef mizuiro::color::homogenous_static<
 	real,
 	mizuiro::color::layout::rgb
 > rgb32f_format;
