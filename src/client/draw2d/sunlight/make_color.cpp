300d40b88f9ba32d53d83614822ab282a3783f45
Philipp Reh <sefi@s-e-f-i.de>
2012-07-15 00:55:38 +0200

    Fixed color::convert.

--- src/client/draw2d/sunlight/make_color.cpp
+++ src/client/draw2d/sunlight/make_color.cpp
@@ -1,9 +1,9 @@
 #include <sanguis/client/draw2d/sunlight/make_color.hpp>
 #include <sanguis/client/draw2d/sunlight/day_gradient.hpp>
 #include <sanguis/client/draw2d/sunlight/gradients.hpp>
+#include <sge/image/color/convert.hpp>
 #include <sge/image/color/rgba32f_format.hpp>
 #include <sge/image/color/any/object.hpp>
-#include <mizuiro/color/convert.hpp>
 #include <fcppt/variant/object_impl.hpp>
 
 sge::image::color::any::object const
@@ -14,7 +14,7 @@ sanguis::client::draw2d::sunlight::make_color(
 	return
 		// convert into something that sge::image::color::any supports
 		sge::image::color::any::object(
-			mizuiro::color::convert<
+			sge::image::color::convert<
 				sge::image::color::rgba32f_format
 			>(
 				day_gradient().interpolate(
