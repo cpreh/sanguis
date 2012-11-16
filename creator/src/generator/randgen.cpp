1dae91415c139be584af5607038ec96bd74c5000
Philipp Reh <sefi@s-e-f-i.de>
2012-11-16 16:07:24 +0100

    Added min/max for generator/randgen

--- creator/src/generator/randgen.cpp
+++ creator/src/generator/randgen.cpp
@@ -1,11 +1,17 @@
 #include <sanguis/creator/generator/randgen.hpp>
 #include <sanguis/creator/generator/seed.hpp>
+#include <fcppt/preprocessor/disable_vc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
 #include <fcppt/random/distribution/uniform_int_impl.hpp>
 #include <fcppt/random/generator/minstd_rand_impl.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <limits>
 #include <fcppt/config/external_end.hpp>
 
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_VC_WARNING(4355)
 
 sanguis::creator::generator::randgen::randgen(
 	sanguis::creator::generator::seed const &_seed
@@ -18,18 +24,16 @@ sanguis::creator::generator::randgen::randgen(
 	),
 	distribution_(
 		sanguis::creator::generator::randgen::int_distribution::min(
-			std::numeric_limits<
-				sanguis::creator::generator::randgen::result_type::value_type
-			>::min()
+			this->min().get()
 		),
 		sanguis::creator::generator::randgen::int_distribution::max(
-			std::numeric_limits<
-				sanguis::creator::generator::randgen::result_type::value_type
-			>::max()
+			this->max().get()
 		)
 	)
 {
 }
+
+FCPPT_PP_POP_WARNING
 
 sanguis::creator::generator::randgen::~randgen()
 {
@@ -43,5 +47,27 @@ sanguis::creator::generator::randgen::operator()()
 			distribution_(
 				generator_
 			)
+		);
+}
+
+sanguis::creator::generator::randgen::result_type const
+sanguis::creator::generator::randgen::min() const
+{
+	return
+		sanguis::creator::generator::randgen::result_type(
+			std::numeric_limits<
+				sanguis::creator::generator::randgen::result_type::value_type
+			>::min()
+		);
+}
+
+sanguis::creator::generator::randgen::result_type const
+sanguis::creator::generator::randgen::max() const
+{
+	return
+		sanguis::creator::generator::randgen::result_type(
+			std::numeric_limits<
+				sanguis::creator::generator::randgen::result_type::value_type
+			>::max()
 		);
 }
