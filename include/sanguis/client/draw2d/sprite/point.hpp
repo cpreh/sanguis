aebabcfcc0840b620027db9c1d0e73f5c6b3a672
Philipp Reh <sefi@s-e-f-i.de>
2013-03-21 19:46:32 +0100

    Fix for sge

--- include/sanguis/client/draw2d/sprite/point.hpp
+++ include/sanguis/client/draw2d/sprite/point.hpp
@@ -2,7 +2,8 @@
 #define SANGUIS_CLIENT_DRAW2D_SPRITE_POINT_HPP_INCLUDED
 
 #include <sanguis/client/draw2d/sprite/type_choices.hpp>
-#include <sge/sprite/vector.hpp>
+#include <sge/sprite/types/vector.hpp>
+
 
 namespace sanguis
 {
@@ -14,7 +15,7 @@ namespace sprite
 {
 
 typedef sge::sprite::types::vector<
-	sprite::type_choices
+	sanguis::client::draw2d::sprite::type_choices
 >::type point;
 
 }
