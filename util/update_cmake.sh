0cacdb2b7f54795f6b760262ad129925bb2116c8
Philipp Reh <sefi@s-e-f-i.de>
2013-09-14 11:23:20 +0200

    Made sge::projectile optional

--- util/update_cmake.sh
+++ util/update_cmake.sh
@@ -12,12 +12,8 @@ pushd creator > /dev/null
 
 update_cmake \
 	CMakeLists.txt \
-	SANGUIS_CREATOR_INCLUDE_FILES \
-	include
-
-update_cmake \
-	CMakeLists.txt \
-	SANGUIS_CREATOR_SRC_FILES \
+	SANGUIS_CREATOR_FILES \
+	include \
 	src
 
 popd > /dev/null
@@ -26,12 +22,24 @@ pushd collision > /dev/null
 
 update_cmake \
 	CMakeLists.txt \
-	SANGUIS_COLLISION_INCLUDE_FILES \
-	include
+	SANGUIS_COLLISION_FILES \
+	include \
+	-n \
+	src \
+	src/include/sanguis/collision/aux \
+	src/aux \
+	src/include/sanguis/collision/aux/world \
+	src/aux/world \
+	-r \
+	src/world \
+	src/include/sanguis/collision/aux/world/simple \
+	src/aux/world/simple
+
 
 update_cmake \
 	CMakeLists.txt \
-	SANGUIS_COLLISION_SRC_FILES \
-	src
+	SANGUIS_COLLISION_PROJECTILE_FILES \
+	src/include/sanguis/collision/aux/world/projectile \
+	src/aux/world/projectile
 
 popd > /dev/null
