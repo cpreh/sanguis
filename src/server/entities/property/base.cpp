731d31d8b23522f63c83a7a011d35134118c6128
Philipp Reh <sefi@s-e-f-i.de>
2013-10-10 17:06:15 +0200

    Ensure that properties are not negative.

--- src/server/entities/property/base.cpp
+++ src/server/entities/property/base.cpp
@@ -2,6 +2,10 @@
 #include <sanguis/server/entities/property/constant.hpp>
 #include <sanguis/server/entities/property/linear.hpp>
 #include <sanguis/server/entities/property/value.hpp>
+#include <fcppt/literal.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <algorithm>
+#include <fcppt/config/external_end.hpp>
 
 
 sanguis::server::entities::property::base::base(
@@ -19,13 +23,15 @@ sanguis::server::entities::property::base::base(
 sanguis::server::entities::property::constant const
 sanguis::server::entities::property::base::constant()
 {
-	return constant_;
+	return
+		constant_;
 }
 
 sanguis::server::entities::property::linear const
 sanguis::server::entities::property::base::linear()
 {
-	return linear_;
+	return
+		linear_;
 }
 
 void
@@ -55,9 +61,15 @@ sanguis::server::entities::property::base::~base()
 void
 sanguis::server::entities::property::base::recalc_max()
 {
-	// TODO: Some linear values shouldn't become negative!
 	this->on_recalc_max(
-		(base_ + constant_.value())
-		* linear_.value()
+		std::max(
+			(base_ + constant_.value())
+			* linear_.value(),
+			fcppt::literal<
+				sanguis::server::entities::property::value
+			>(
+				0
+			)
+		)
 	);
 }
