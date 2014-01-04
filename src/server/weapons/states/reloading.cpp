36be726d3a344eca0f9e66ef4db47dba62e540d2
Philipp Reh <sefi@s-e-f-i.de>
2014-01-04 23:12:08 +0100

    Added missing include for clang

--- src/server/weapons/states/reloading.cpp
+++ src/server/weapons/states/reloading.cpp
@@ -2,6 +2,7 @@
 #include <sanguis/weapon_status.hpp>
 #include <sanguis/server/weapons/weapon.hpp>
 #include <sanguis/server/weapons/events/poll.hpp>
+#include <sanguis/server/weapons/events/reload.hpp>
 #include <sanguis/server/weapons/events/shoot.hpp>
 #include <sanguis/server/weapons/events/stop.hpp>
 #include <sanguis/server/weapons/states/reloading.hpp>
