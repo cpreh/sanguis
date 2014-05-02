5ef32deecbed011d92b35194bec203b58fb438b2
Philipp Reh <sefi@s-e-f-i.de>
2014-05-02 16:00:39 +0200

    Also try to place multiple enemies

--- server/src/server/world/generate_single_spawns.cpp
+++ server/src/server/world/generate_single_spawns.cpp
@@ -1,19 +1,20 @@
 #include <sanguis/random_generator_fwd.hpp>
 #include <sanguis/creator/spawn_pos.hpp>
 #include <sanguis/creator/spawn_type.hpp>
+#include <sanguis/load/enemy_name.hpp>
+#include <sanguis/server/dim.hpp>
+#include <sanguis/server/model_name.hpp>
 #include <sanguis/server/entities/auto_weak_link.hpp>
 #include <sanguis/server/entities/base.hpp>
-#include <sanguis/server/entities/insert_parameters_center.hpp>
 #include <sanguis/server/entities/spawn_owner.hpp>
 #include <sanguis/server/entities/enemies/create.hpp>
 #include <sanguis/server/entities/enemies/special_chance.hpp>
-#include <sanguis/server/environment/load_context_fwd.hpp>
+#include <sanguis/server/environment/load_context.hpp>
 #include <sanguis/server/world/difficulty.hpp>
 #include <sanguis/server/world/generate_single_spawns.hpp>
-#include <sanguis/server/world/grid_pos_to_center.hpp>
-#include <sanguis/server/world/insert_pair.hpp>
 #include <sanguis/server/world/insert_pair_container.hpp>
-#include <fcppt/assign/make_container.hpp>
+#include <sanguis/server/world/place_callback.hpp>
+#include <sanguis/server/world/place_multiple.hpp>
 
 
 sanguis::server::world::insert_pair_container
@@ -25,29 +26,38 @@ sanguis::server::world::generate_single_spawns(
 	sanguis::server::world::difficulty const _difficulty
 )
 {
-	// TODO
 	return
-		fcppt::assign::make_container<
-			sanguis::server::world::insert_pair_container
-		>(
-			sanguis::server::world::insert_pair(
-				sanguis::server::entities::enemies::create(
-					_random_generator,
+		sanguis::server::world::place_multiple(
+			_random_generator,
+			sanguis::server::world::place_callback(
+				[
+					&_random_generator,
 					_type,
 					_difficulty,
-					_load_context,
-					sanguis::server::entities::spawn_owner(
-						sanguis::server::entities::auto_weak_link()
-					),
-					sanguis::server::entities::enemies::special_chance(
-						0.05f
-					)
-				),
-				sanguis::server::entities::insert_parameters_center(
-					sanguis::server::world::grid_pos_to_center(
-						_pos.get()
+					&_load_context]
+				{
+					return
+						sanguis::server::entities::enemies::create(
+							_random_generator,
+							_type,
+							_difficulty,
+							_load_context,
+							sanguis::server::entities::spawn_owner(
+								sanguis::server::entities::auto_weak_link()
+							),
+							sanguis::server::entities::enemies::special_chance(
+								0.05f
+							)
+						);
+				}
+			),
+			_pos.get(),
+			_load_context.entity_dim(
+				sanguis::server::model_name(
+					sanguis::load::enemy_name(
+						_type
 					)
 				)
 			)
-		).move_container();
+		);
 }
