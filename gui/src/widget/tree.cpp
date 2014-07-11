b7dfecaab5db846f55857513b729794c95f66b95
Philipp Reh <sefi@s-e-f-i.de>
2014-07-11 17:16:44 +0200

    Use map_concat

--- gui/src/widget/tree.cpp
+++ gui/src/widget/tree.cpp
@@ -23,16 +23,12 @@
 #include <fcppt/literal.hpp>
 #include <fcppt/make_unique_ptr.hpp>
 #include <fcppt/noncopyable.hpp>
-#include <fcppt/algorithm/append_result.hpp>
-#include <fcppt/algorithm/fold.hpp>
 #include <fcppt/algorithm/map.hpp>
+#include <fcppt/algorithm/map_concat_move.hpp>
 #include <fcppt/cast/size.hpp>
 #include <fcppt/cast/to_signed.hpp>
 #include <fcppt/container/tree/level.hpp>
 #include <fcppt/container/tree/pre_order.hpp>
-#include <fcppt/config/external_begin.hpp>
-#include <utility>
-#include <fcppt/config/external_end.hpp>
 
 
 sanguis::gui::widget::tree::tree(
@@ -47,133 +43,128 @@ sanguis::gui::widget::tree::tree(
 		sanguis::gui::default_aspect()
 	),
 	boxes_(
-		fcppt::algorithm::fold(
+		fcppt::algorithm::map_concat_move<
+			sanguis::gui::widget::unique_ptr_vector
+		>(
 			_widgets,
-			sanguis::gui::widget::unique_ptr_vector(),
 			[
 				&_context
 			](
-				sanguis::gui::widget::reference_tree const &_tree,
-				sanguis::gui::widget::unique_ptr_vector &&_state
+				sanguis::gui::widget::reference_tree const &_tree
 			)
 			{
 				return
-					fcppt::algorithm::append_result(
-						std::move(
-							_state
-						),
-						fcppt::algorithm::map<
-							sanguis::gui::widget::unique_ptr_vector
+					fcppt::algorithm::map<
+						sanguis::gui::widget::unique_ptr_vector
+					>(
+						fcppt::container::tree::pre_order<
+							sanguis::gui::widget::reference_tree const
 						>(
-							fcppt::container::tree::pre_order<
-								sanguis::gui::widget::reference_tree const
-							>(
-								_tree
-							),
-							[
-								&_context
-							](
-								sanguis::gui::widget::reference_tree const &_widget
-							)
-							{
-								class indented_widget
-								:
-									public sanguis::gui::widget::box_container
-								{
-									FCPPT_NONCOPYABLE(
-										indented_widget
-									);
-								public:
-									indented_widget(
-										sge::rucksack::scalar const _indent,
-										sanguis::gui::context &_new_context,
-										sanguis::gui::widget::base &_new_widget
-									)
-									:
-										sanguis::gui::widget::box_container(
-											_new_context,
-											sanguis::gui::widget::reference_alignment_vector{
-												sanguis::gui::widget::reference_alignment_pair(
-													sanguis::gui::widget::reference(
-														_new_widget
-													),
-													sge::rucksack::alignment::left_or_top
-												)
-											},
-											sge::rucksack::axis::x,
-											sanguis::gui::default_aspect()
-										),
-										padding_(
-											sge::rucksack::axis_policy2(
-												sge::rucksack::axis_policy(
-													sge::rucksack::minimum_size(
-														_indent
-													),
-													sge::rucksack::preferred_size(
-														sge::rucksack::optional_scalar(
-															_indent
-														)
-													),
-													sge::rucksack::is_expanding(
-														false
-													)
-												),
-												sge::rucksack::axis_policy(
-													sge::rucksack::minimum_size(
-														fcppt::literal<
-															sge::rucksack::scalar
-														>(
-															0
-														)
-													),
-													sge::rucksack::preferred_size(
-														sge::rucksack::optional_scalar()
-													),
-													sge::rucksack::is_expanding(
-														false
-													)
-												),
-												sanguis::gui::default_aspect()
-											)
-										)
-									{
-										this->box_layout().push_front_child(
-											padding_,
-											sge::rucksack::alignment::left_or_top
-										);
-									}
-
-									~indented_widget()
-									{
-									}
-								private:
-									sge::rucksack::widget::dummy padding_;
-								};
-
-								return
-									fcppt::make_unique_ptr<
-										indented_widget
-									>(
-										fcppt::cast::size<
-											sge::rucksack::scalar
-										>(
-											fcppt::cast::to_signed(
-												fcppt::container::tree::level(
-													_widget
-												)
-											)
-										)
-										*
-										fcppt::literal<
-											sge::rucksack::scalar
-										>(
-											20
-										),
-										_context,
-										_widget.value().get()
-									);
-							}
+							_tree
+						),
+						[
+							&_context
+						](
+							sanguis::gui::widget::reference_tree const &_widget
 						)
+						{
+							class indented_widget
+							:
+								public sanguis::gui::widget::box_container
+							{
+								FCPPT_NONCOPYABLE(
+									indented_widget
+								);
+							public:
+								indented_widget(
+									sge::rucksack::scalar const _indent,
+									sanguis::gui::context &_new_context,
+									sanguis::gui::widget::base &_new_widget
+								)
+								:
+									sanguis::gui::widget::box_container(
+										_new_context,
+										sanguis::gui::widget::reference_alignment_vector{
+											sanguis::gui::widget::reference_alignment_pair(
+												sanguis::gui::widget::reference(
+													_new_widget
+												),
+												sge::rucksack::alignment::left_or_top
+											)
+										},
+										sge::rucksack::axis::x,
+										sanguis::gui::default_aspect()
+									),
+									padding_(
+										sge::rucksack::axis_policy2(
+											sge::rucksack::axis_policy(
+												sge::rucksack::minimum_size(
+													_indent
+												),
+												sge::rucksack::preferred_size(
+													sge::rucksack::optional_scalar(
+														_indent
+													)
+												),
+												sge::rucksack::is_expanding(
+													false
+												)
+											),
+											sge::rucksack::axis_policy(
+												sge::rucksack::minimum_size(
+													fcppt::literal<
+														sge::rucksack::scalar
+													>(
+														0
+													)
+												),
+												sge::rucksack::preferred_size(
+													sge::rucksack::optional_scalar()
+												),
+												sge::rucksack::is_expanding(
+													false
+												)
+											),
+											sanguis::gui::default_aspect()
+										)
+									)
+								{
+									this->box_layout().push_front_child(
+										padding_,
+										sge::rucksack::alignment::left_or_top
+									);
+								}
+
+								~indented_widget()
+								{
+								}
+							private:
+								sge::rucksack::widget::dummy padding_;
+							};
+
+							return
+								fcppt::make_unique_ptr<
+									indented_widget
+								>(
+									fcppt::cast::size<
+										sge::rucksack::scalar
+									>(
+										fcppt::cast::to_signed(
+											fcppt::container::tree::level(
+												_widget
+											)
+										)
+									)
+									*
+									fcppt::literal<
+										sge::rucksack::scalar
+									>(
+										20
+									),
+									_context,
+									_widget.value().get()
+								);
+						}
 					);
 			}
 		)
--- server/src/server/send_available_perks.cpp
+++ server/src/server/send_available_perks.cpp
@@ -16,13 +16,9 @@
 #include <sanguis/server/entities/player.hpp>
 #include <sanguis/server/perks/tree/object.hpp>
 #include <sanguis/server/perks/tree/status.hpp>
-#include <fcppt/algorithm/fold.hpp>
-#include <fcppt/algorithm/join.hpp>
 #include <fcppt/algorithm/map.hpp>
+#include <fcppt/algorithm/map_concat.hpp>
 #include <fcppt/container/tree/pre_order.hpp>
-#include <fcppt/config/external_begin.hpp>
-#include <utility>
-#include <fcppt/config/external_end.hpp>
 
 
 void
@@ -32,58 +28,53 @@ sanguis::server::send_available_perks(
 )
 {
 	sanguis::messages::server::types::perk_tree_node_vector const nodes(
-		fcppt::algorithm::fold(
+		fcppt::algorithm::map_concat<
+			sanguis::messages::server::types::perk_tree_node_vector
+		>(
 			_player.perk_tree(),
-			sanguis::messages::server::types::perk_tree_node_vector(),
 			[](
-				sanguis::server::perks::tree::object const &_tree,
-				sanguis::messages::server::types::perk_tree_node_vector &&_state
+				sanguis::server::perks::tree::object const &_tree
 			)
 			{
 				return
-					fcppt::algorithm::join(
-						std::move(
-							_state
-						),
-						fcppt::algorithm::map<
-							sanguis::messages::server::types::perk_tree_node_vector
+					fcppt::algorithm::map<
+						sanguis::messages::server::types::perk_tree_node_vector
+					>(
+						fcppt::container::tree::pre_order<
+							sanguis::server::perks::tree::object const
 						>(
-							fcppt::container::tree::pre_order<
-								sanguis::server::perks::tree::object const
-							>(
-								_tree
-							),
-							[](
-								sanguis::server::perks::tree::object const &_inner
-							)
-							{
-								sanguis::server::perks::tree::status const &info(
-									_inner.value()
-								);
-
-								return
-									sanguis::messages::server::types::perk_tree_node(
-										sanguis::messages::roles::perk_label{} =
-											info.type(),
-										sanguis::messages::roles::perk_level{} =
-											info.level().get(),
-										sanguis::messages::roles::required_perk_player_level{} =
-											info.required_player_level().get(),
-										sanguis::messages::roles::required_perk_parent_level{} =
-											info.required_parent_level().get(),
-										sanguis::messages::roles::max_perk_level{} =
-											info.max_level().get(),
-										sanguis::messages::roles::perk_parent{} =
-											_inner.parent().has_value()
-											?
-												sanguis::optional_perk_type(
-													_inner.parent()->value().type()
-												)
-											:
-												sanguis::optional_perk_type()
-									);
-							}
+							_tree
+						),
+						[](
+							sanguis::server::perks::tree::object const &_inner
 						)
+						{
+							sanguis::server::perks::tree::status const &info(
+								_inner.value()
+							);
+
+							return
+								sanguis::messages::server::types::perk_tree_node(
+									sanguis::messages::roles::perk_label{} =
+										info.type(),
+									sanguis::messages::roles::perk_level{} =
+										info.level().get(),
+									sanguis::messages::roles::required_perk_player_level{} =
+										info.required_player_level().get(),
+									sanguis::messages::roles::required_perk_parent_level{} =
+										info.required_parent_level().get(),
+									sanguis::messages::roles::max_perk_level{} =
+										info.max_level().get(),
+									sanguis::messages::roles::perk_parent{} =
+										_inner.parent().has_value()
+										?
+											sanguis::optional_perk_type(
+												_inner.parent()->value().type()
+											)
+										:
+											sanguis::optional_perk_type()
+								);
+						}
 					);
 			}
 		)
