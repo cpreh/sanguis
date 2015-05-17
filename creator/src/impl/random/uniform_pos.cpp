8dc6c87c3e085b4f79554b7cd50aaf84e9102211
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-05-17 17:21:46 +0200

    Call random_x and random_y in order

--- creator/src/impl/random/uniform_pos.cpp
+++ creator/src/impl/random/uniform_pos.cpp
@@ -1,5 +1,6 @@
 #include <sanguis/creator/dim.hpp>
 #include <sanguis/creator/pos.hpp>
+#include <sanguis/creator/size_type.hpp>
 #include <sanguis/creator/impl/random/generator_fwd.hpp>
 #include <sanguis/creator/impl/random/uniform_pos.hpp>
 #include <sanguis/creator/impl/random/uniform_pos_parameters.hpp>
@@ -28,9 +29,17 @@ sanguis::creator::impl::random::uniform_pos::uniform_pos(
 sanguis::creator::pos
 sanguis::creator::impl::random::uniform_pos::operator()()
 {
+	sanguis::creator::size_type const pos_x(
+		random_x_()
+	);
+
+	sanguis::creator::size_type const pos_y(
+		random_y_()
+	);
+
 	return
 		sanguis::creator::pos(
-			random_x_(),
-			random_y_()
+			pos_x,
+			pos_y
 		);
 }
