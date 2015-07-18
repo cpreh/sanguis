c8debe08356e298245e42a5da71a2fbdbd3ffaf4
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-07-18 16:48:49 +0200

    Added creator::impl::blit

---
+++ creator/src/impl/blit.cpp
@@ -0,0 +1,102 @@
+#include <sanguis/creator/enable_if_tile.hpp>
+#include <sanguis/creator/instantiate_tile.hpp>
+#include <sanguis/creator/tile_grid.hpp>
+#include <sanguis/creator/impl/blit.hpp>
+#include <fcppt/assert/pre.hpp>
+#include <fcppt/container/grid/pos_ref_range.hpp>
+#include <fcppt/container/grid/pos_reference.hpp>
+#include <fcppt/container/grid/range_dim.hpp>
+#include <fcppt/math/dim/comparison.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/range/combine.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <fcppt/config/external_end.hpp>
+
+
+template<
+	typename Tile
+>
+sanguis::creator::enable_if_tile<
+	Tile,
+	void
+>
+sanguis::creator::impl::blit(
+	fcppt::container::grid::pos_ref_range<
+		sanguis::creator::tile_grid<
+			Tile
+		>
+	> const _dest,
+	fcppt::container::grid::pos_ref_range<
+		sanguis::creator::tile_grid<
+			Tile
+		> const
+	> const _source
+)
+{
+	FCPPT_ASSERT_PRE(
+		fcppt::container::grid::range_dim(
+			_dest.min(),
+			_dest.sup()
+		)
+		==
+		fcppt::container::grid::range_dim(
+			_source.min(),
+			_source.sup()
+		)
+	);
+
+	for(
+		boost::tuple<
+			fcppt::container::grid::pos_reference<
+				sanguis::creator::tile_grid<
+					Tile
+				>
+			>,
+			fcppt::container::grid::pos_reference<
+				sanguis::creator::tile_grid<
+					Tile
+				> const
+			>
+		> const &element
+		:
+		boost::range::combine(
+			_dest,
+			_source
+		)
+	)
+		boost::get<
+			0
+		>(
+			element
+		).value() =
+			boost::get<
+				1
+			>(
+				element
+			).value();
+}
+
+#define SANGUIS_CREATOR_INSTANTIATE_BLIT(\
+	tile_type\
+)\
+template \
+sanguis::creator::enable_if_tile<\
+	tile_type, \
+	void \
+> \
+sanguis::creator::impl::blit( \
+	fcppt::container::grid::pos_ref_range< \
+		sanguis::creator::tile_grid< \
+			tile_type \
+		> \
+	>, \
+	fcppt::container::grid::pos_ref_range< \
+		sanguis::creator::tile_grid< \
+			tile_type \
+		> const \
+	> \
+)
+
+SANGUIS_CREATOR_INSTANTIATE_TILE(
+	SANGUIS_CREATOR_INSTANTIATE_BLIT
+);
---
+++ creator/src/include/sanguis/creator/impl/blit.hpp
@@ -0,0 +1,40 @@
+#ifndef SANGUIS_CREATOR_IMPL_BLIT_HPP_INCLUDED
+#define SANGUIS_CREATOR_IMPL_BLIT_HPP_INCLUDED
+
+#include <sanguis/creator/enable_if_tile.hpp>
+#include <sanguis/creator/tile_grid_fwd.hpp>
+#include <fcppt/container/grid/pos_ref_range_fwd.hpp>
+
+
+namespace sanguis
+{
+namespace creator
+{
+namespace impl
+{
+
+template<
+	typename Tile
+>
+sanguis::creator::enable_if_tile<
+	Tile,
+	void
+>
+blit(
+	fcppt::container::grid::pos_ref_range<
+		sanguis::creator::tile_grid<
+			Tile
+		>
+	>,
+	fcppt::container::grid::pos_ref_range<
+		sanguis::creator::tile_grid<
+			Tile
+		> const
+	>
+);
+
+}
+}
+}
+
+#endif
