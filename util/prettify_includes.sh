096b402bd2a1f3f6c6004acd4bcf8e5230283ff2
Carl Philipp Reh <carlphilippreh@gmail.com>
2020-02-01 21:45:59 +0100

    Update prettify_includes

--- util/prettify_includes.sh
+++ util/prettify_includes.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-ARGS=(--library sanguis --library alda --library sge --library awl --library mizuiro --library fcppt)
+LIBS=(--library sanguis --library alda --library sge --library awl --library mizuiro --library fcppt)
+DIRS=(client collision common core creator main media model server tools)
 
-find client collision common core creator main media model server tools \( \( -name '*.hpp' -o -name '*.cpp' \) \) -exec update_headers.sh '{}' "${ARGS[@]}" \;
+prettify_includes "${LIBS[@]}" "${DIRS[@]}"
