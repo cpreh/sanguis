755dd8ea53400d68b96ee9c46b8c8f74d445e293
Carl Philipp Reh <carlphilippreh@gmail.com>
2024-01-25 16:36:00 +0100

    Ignore another clang-tidy warning

--- common/include/sanguis/state_override.hpp
+++ common/include/sanguis/state_override.hpp
@@ -1,7 +1,7 @@
 #ifndef SANGUIS_STATE_OVERRIDE_HPP_INCLUDED
 #define SANGUIS_STATE_OVERRIDE_HPP_INCLUDED
 
-#include <fcppt/config/compiler.hpp>
+#include <fcppt/config/compiler.hpp> // IWYU pragma: keep
 
 #if defined(FCPPT_CONFIG_MSVC_COMPILER)
 #define SANGUIS_STATE_OVERRIDE
