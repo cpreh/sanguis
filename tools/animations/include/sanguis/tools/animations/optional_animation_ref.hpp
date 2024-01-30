9f11d4a993eca7fce71bedec6cc2b4a8f1326fbc
Carl Philipp Reh <carlphilippreh@gmail.com>
2024-01-30 17:43:48 +0100

    Ignore another clang-tidy warning

--- tools/animations/include/sanguis/tools/animations/optional_animation_ref.hpp
+++ tools/animations/include/sanguis/tools/animations/optional_animation_ref.hpp
@@ -2,6 +2,6 @@
 #define SANGUIS_TOOLS_ANIMATIONS_OPTIONAL_ANIMATION_REF_HPP_INCLUDED
 
 #include <sanguis/tools/animations/optional_animation_ref_fwd.hpp> // IWYU pragma: export
-#include <fcppt/optional/reference.hpp>
+#include <fcppt/optional/reference.hpp> // IWYU pragma: keep
 
 #endif
