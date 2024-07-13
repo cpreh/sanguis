652dfbf7551f665c0da48281e62be6400dbd8859
Carl Philipp Reh <carlphilippreh@gmail.com>
2024-07-13 16:19:13 +0200

    Use more unordered maps

--- client/include/sanguis/client/load/hud/weapon_icon_map.hpp
+++ client/include/sanguis/client/load/hud/weapon_icon_map.hpp
@@ -5,14 +5,15 @@
 #include <sge/texture/part_fwd.hpp>
 #include <fcppt/reference.hpp>
 #include <fcppt/variant/comparison.hpp> // IWYU pragma: keep
+#include <fcppt/variant/std_hash.hpp> // IWYU pragma: keep
 #include <fcppt/config/external_begin.hpp>
-#include <map>
+#include <unordered_map>
 #include <fcppt/config/external_end.hpp>
 
 namespace sanguis::client::load::hud
 {
 
-using weapon_icon_map = std::map<sanguis::weapon_type, fcppt::reference<sge::texture::part const>>;
+using weapon_icon_map = std::unordered_map<sanguis::weapon_type, fcppt::reference<sge::texture::part const>>;
 
 }
 
--- client/include/sanguis/client/load/model/weapon_category_map.hpp
+++ client/include/sanguis/client/load/model/weapon_category_map.hpp
@@ -4,14 +4,15 @@
 #include <sanguis/optional_primary_weapon_type.hpp>
 #include <sanguis/client/load/model/weapon_category_unique_ptr.hpp>
 #include <fcppt/optional/comparison.hpp> // IWYU pragma: keep
+#include <fcppt/optional/std_hash.hpp> // IWYU pragma: keep
 #include <fcppt/config/external_begin.hpp>
-#include <map>
+#include <unordered_map>
 #include <fcppt/config/external_end.hpp>
 
 namespace sanguis::client::load::model
 {
 
-using weapon_category_map = std::map<
+using weapon_category_map = std::unordered_map<
     sanguis::optional_primary_weapon_type,
     sanguis::client::load::model::weapon_category_unique_ptr>;
 
