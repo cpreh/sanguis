3588c129e579f43f9f44cc16e5250ac952eb240f
Carl Philipp Reh <carlphilippreh@gmail.com>
2024-07-14 14:12:32 +0200

    Add missing includes

--- server/src/server/global/source_world_pair_hash.cpp
+++ server/src/server/global/source_world_pair_hash.cpp
@@ -1,5 +1,7 @@
 #include <sanguis/server/global/source_world_pair.hpp>
 #include <sanguis/server/global/source_world_pair_hash.hpp>
+#include <fcppt/hash.hpp>
+#include <fcppt/hash_combine.hpp>
 #include <fcppt/strong_typedef_std_hash.hpp> // IWYU pragma: keep
 #include <fcppt/math/vector/std_hash.hpp> // IWYU pragma: keep
 #include <fcppt/config/external_begin.hpp>
